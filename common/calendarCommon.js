1982591a7882a3207260be2ba2079681da35ea1a
Charon <1635576341@qq.com>
2024-12-15 22:27:48 +0800

    添加注释

--- common/calendarCommon.js
+++ common/calendarCommon.js
@@ -1,7 +1,11 @@
 var WEEK_DATE_COUNT = 7;
-// 定义可用的日期数组
-const USE_DATES = ['2024-12-5', '2024-12-6', '2024-12-7','2024-12-9', '2024-12-10'];
 
+/**
+ * 格式化日期时间
+ * 将给定的日期对象转换为指定格式的字符串
+ * @param {Date} date - 日期对象，用于获取年月日信息
+ * @returns {string} - 格式化后的日期字符串，格式为YYYY-MM-DD
+ */
 function formatDate(date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1); 
@@ -9,12 +13,26 @@ function formatDate(date) {
   return `${year}-${month}-${day}`;
 }
 
+/**
+ * 获取指定年份和月份的天数
+ * 
+ * @param {number} year - 指定的年份，例如2023
+ * @param {number} month - 指定的月份，月份是从0开始的，所以1月是0，12月是11
+ * @returns {number} 返回指定月份的天数
+ */
 function getDateOfMonth(year, month) {
   var dateInstance = new Date(year, month, 0);
   var dateCount = dateInstance.getDate();
   return dateCount;
 }
-//上个月需要多少天
+/**
+ * 获取指定年份和月份的第一天是星期几
+ * 返回值为0至6的整数，分别代表星期日到星期六
+ * 
+ * @param {number} year - 指定的年份，例如2023
+ * @param {number} month - 指定的月份，1至12代表一月到十二月
+ * @returns {number} - 0为星期日，1为星期一
+ */
 function getDateOfMonthFirstDate(year, month) {
   var dateInstance = new Date(year, month - 1, 1);
   var day = dateInstance.getDay();
@@ -33,17 +51,18 @@ function createDateCellFragment(start, end, year, month) {
     var currentValue = calendarDateElement.calendarInfo;
     if(currentValue === formatDate(date)){
       calendarDateElement.classList.add('calendar-date_base_now_date_style');
-    }else if (!USE_DATES.includes(currentValue)) {
+    }
+    if (!USE_DATES.includes(currentValue)) {
       calendarDateElement.classList.add('disabled');
     }else{
       calendarDateElement.classList.add('calendar-date_base_canClick')
     }
     fragment.appendChild(calendarDateElement);
-
   }
   return fragment;
 }
 
+// 修改日历的年份跟月份
 function changeYearAndMonth(newYear, newMonth, bodyEle) {
   var value = document.querySelector(".calendar_bar_date");
   value.innerText = `${newYear}年${newMonth}月`;
